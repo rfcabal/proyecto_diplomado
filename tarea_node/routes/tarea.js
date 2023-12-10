1d987965a4ae5712dabe48096e0313bcbe05d59d
Ricardo Flores <rf.cabal@gmail.com>
2023-12-10 20:21:05 -0300

    se agrega tarea 2 de node

--- tarea_node/routes/tarea.js
+++ tarea_node/routes/tarea.js
@@ -5,6 +5,14 @@ router.get('/', function(req, res, next) {
   res.send('esto es una prueba de GET');
 });
 
+router.get('/bienvenido', function(req, res, next) {
+
+  const nombre = req.query.nombre || ''
+
+  res.send(`Hola ${nombre}`);
+
+});
+
 router.post('/', function(req, res, next) {
     res.send('esto es una prueba de POST');
 });
