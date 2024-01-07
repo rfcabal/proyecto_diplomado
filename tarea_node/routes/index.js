a1dd2212851ebcadb554d3086b9dd4e12a87bda7
Ricardo Flores <rf.cabal@gmail.com>
2024-01-06 21:22:12 -0300

    se agrega tarea de conexion de sql

--- tarea_node/routes/index.js
+++ tarea_node/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var pool =  require('../config/db')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  pool.query("SELECT * FROM empleados").then( function(resultados) {
+    console.log(resultados);
+  })
+
   res.render('index', { title: 'Express' });
 });
 
